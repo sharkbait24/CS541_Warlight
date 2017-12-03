d3e31d624ba08f02cfbc4d74449d35e2f2ca56f3
Selmon Afzal <selmon754@gmail.com>
2017-12-02 20:04:13 -0800

    place armies for turtle

--- turtlebot.py
+++ turtlebot.py
@@ -15,7 +15,8 @@
 # work with our modified map and split from the bot class to
 # provide a convenient way for us to create new AIs
 
-from bot import Bot, PlaceArmyBuilder, AttackTransferBuilder
+from map import Map
+from bot import Bot, PlaceArmyBuilder, AttackTransferBuilder, PickStartingBuilder
 from const import PLACE_ARMIES, ATTACK_TRANSFER, NO_MOVES
 from math import fmod, pi
 from time import clock
@@ -36,10 +37,11 @@ class TurtleBot(Bot):
         LOCATION FOR PLACING TROOPS '''
     
     def pick_starting_regions(self, options):
-        options = options[1:]
-        ordered_regions = Sorter.sorting(options, self)
-#        shuffled_regions = MyRandom.shuffle(options)
-        return ' '.join(ordered_regions[:6])
+        option = self.parse_pick_starting_regions(options)
+        ordered_regions = Sorter.sorting(option, self, False) # false means ascending order
+        builder = PickStartingBuilder()
+        builder.add_all(ordered_regions[:6])
+        return builder.to_string()
 
     # Places up to 2 armies on random regions
     ''' REPLACE SHUFFLED_REGIONS WITH TUPLE FOR split_last_update WHICH SPLITS 
@@ -51,22 +53,43 @@ class TurtleBot(Bot):
         troops_remaining = self.available_armies
         owned_regions = self.map.get_owned_regions(self.name)  # returns a copy of references to owned regions
         owned, neighbors, outliers = self.map.split_last_update(self.name)
+        
+        in_super = set()
+        
+        for region in owned:
+            in_super.add(region.super_region)
+        
+        in_super = list(in_super)
+        
+        new_in_super = [(x,len(x.regions) - len(Map.get_owned_in_list(x.regions, self.name))) for x in in_super]
+
         shuffled_regions = MyRandom.shuffle(owned_regions)
         
+        new_in_super.sort(key=lambda super_region: super_region[1] )
+       
+
+
         while troops_remaining:
-            region = shuffled_regions[region_index]
-            #if self.turn_elapsed == 1
-            #else:
-            if troops_remaining > 1:
-                placements.add(region.id, 2)
-                region.troop_count += 2
-                troops_remaining -= 2
+
+            if self.turn_elapsed == 1:
+                owned = Sorter.sorting(owned, self, False)
+                best = owned[0]
+                placements.add(best.id, troops_remaining)
+                troops_remaining = 0
+
             else:
-                placements.add(region.id, 1)
-                region.troop_count += 1
-                troops_remaining -= 1
-            region_index += 1
-        #self.turn_elapsed = self.turn_elapsed + 1
+                
+                for x in new_in_super:
+                    if x[1] != 0:
+                        for y in x[0].regions:
+                            if y.owner != self.name:
+                                owned_neighbors = Map.get_owned_in_list(y.neighbors, self.name)
+                                for z in owned_neighbors:
+                                    if troops_remaining > 0:
+                                        placements.add(z.id, 1)
+                                        troops_remaining -= 1
+                
+        self.turn_elapsed = self.turn_elapsed + 1
         return placements.to_string()
 
     # Currently checks whether a region has more than six troops placed to attack,
@@ -87,4 +110,25 @@ class TurtleBot(Bot):
                 else:
                     neighbors.remove(target_region)
         return attack_transfers.to_string()
+
+
+
+class MyRandom(object):
+    @staticmethod
+    def randrange(r_min, r_max):
+        # A pseudo random number generator to replace random.randrange
+        #
+        # Works with an inclusive left bound and exclusive right bound.
+        # E.g. Random.randrange(0, 5) in [0, 1, 2, 3, 4] is always true
+        return r_min + int(fmod(pow(clock() + pi, 2), 1.0) * (r_max - r_min))
+
+    @staticmethod
+    def shuffle(items):
+        # Method to shuffle a list of items
+        i = len(items)
+        while i > 1:
+            i -= 1
+            j = MyRandom.randrange(0, i)
+            items[j], items[i] = items[i], items[j]
+        return items
 
